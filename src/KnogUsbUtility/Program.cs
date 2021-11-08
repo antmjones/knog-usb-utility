8d3aa2c6b6c03d3da23a5c2c3e4339334072782a
Anthony Jones <ant@cantab.net>
2021-11-08 20:52:38 +0000

    Add a "proper" CLI

--- src/KnogUsbUtility/Program.cs
+++ src/KnogUsbUtility/Program.cs
@@ -1,43 +1,66 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text.Json;
+using CommandLine;
 
 namespace KnogUsbUtility {
 
     public static class Program {
-        private static readonly string JsonFileName = "current-config.json";
-        private static readonly string MemoryDumpFileName = "memory-dump.txt";
+        public static int Main(string[] args) =>
+            Parser.Default
+                .ParseArguments<CommandLineOptions>(args)
+                .MapResult(Run, _ => 1);
 
-        public static void Main() =>
-            DownloadAndDumpToJson();
+        private static int Run(CommandLineOptions options) {
+            try {
+                if (!string.IsNullOrEmpty(options.DumpMemoryToFileName)) {
+                    DumpMemory(options.DumpMemoryToFileName);
+                } else if (!string.IsNullOrEmpty(options.ExportToFileName)) {
+                    ExportToJson(options.ExportToFileName);
+                } else if (!string.IsNullOrEmpty(options.ImportFromFileName)) {
+                    ImportFromJson(options.ImportFromFileName);
+                }
 
-        private static void DumpMemory() {
-            using LightConfigurationUploader uploader = LightConfigurationUploader.Create();
-            using TextWriter writer = new StreamWriter(MemoryDumpFileName);
-            uploader.DumpMemory(writer);
+                return 0;
+            } catch (Exception ex) {
+                Console.Error.WriteLine(ex);
+                return -1;
+            }
         }
 
-        private static void UploadFromJson() {
+        private static void DumpMemory(string toFileName) {
+            using LightConfigurationUploader uploader = LightConfigurationUploader.Create();
+            using TextWriter writer = new StreamWriter(toFileName);
+            uploader.DumpMemory(writer);
+
+            Console.Write("Memory dumped to: " + toFileName);
+        }
+
+        private static void ImportFromJson(string fromFileName) {
             LightConfiguration? config =
-                JsonSerializer.Deserialize<LightConfiguration>(File.ReadAllText(JsonFileName));
+                JsonSerializer.Deserialize<LightConfiguration>(File.ReadAllText(fromFileName));
 
             if (config == null) {
-                throw new InvalidDataException("Could not read " + JsonFileName);
+                throw new InvalidDataException("Could not read " + fromFileName);
             }
 
             using LightConfigurationUploader uploader = LightConfigurationUploader.Create();
             uploader.Upload(config);
+
+            Console.Write("Configuration imported from: " + fromFileName);
         }
 
-        private static void DownloadAndDumpToJson() {
+        private static void ExportToJson(string toFileName) {
             using LightConfigurationUploader uploader = LightConfigurationUploader.Create();
-
             LightConfiguration config = uploader.Download();
 
             string result = JsonSerializer.Serialize(config, new JsonSerializerOptions {
                 WriteIndented = true,
             });
 
-            File.WriteAllText("current-config.json", result);
+            File.WriteAllText(toFileName, result);
+
+            Console.Write("Configuration exported to: " + toFileName);
         }
     }
 }
